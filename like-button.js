557b3d93af2becf48c94ff03891d04b9133e1a37
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-14 16:45:59 +0530

    jsx in html

--- like-button.js
+++ like-button.js
@@ -1,26 +1,23 @@
 'use strict';
 
-function LikeButton(props) {
+function LikeButton() {
   const [liked, setLiked] = React.useState(false);
 
   if (liked) {
-    return 'You liked comment number ' + props.commentID;
+    return 'You liked this!';
   }
 
   return React.createElement(
     'button',
-    { onClick: () => setLiked(true) },
+    {
+      onClick: () => setLiked(true),
+    },
     'Like'
   );
 }
 
-// Find all DOM containers, and render Like buttons into them.
-// Note we're using CSS classes instead of IDs so that we can find several nodes.
-document.querySelectorAll('.like-button-root').forEach((rootNode) => {
-  const root = ReactDOM.createRoot(rootNode);
-  // Read the comment ID from a data-* attribute.
-  const commentID = parseInt(rootNode.dataset.commentid, 10);
-  root.render(
-    React.createElement(LikeButton, { commentID: commentID })
-  );
-});
+const rootNode = document.getElementById('like-button-root');
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(LikeButton));
+
+const rootNode2 = document
