95a52ecb34fe14a7f7a8b265891982715f7f241d
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-16 10:40:42 +0530

    funaction and class base component with createRoot method

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 // import React from 'react';
-// import ReactDOM from 'react-dom';
-const React = require('react')
-const ReactDOM = require('react-dom');
+// import ReactDOM from 'react-dom/client';
 
-class App extends React.Component{
-    render() {
-      return <h1>Hello, World!</h1>;
-  }
-}
-
-//function base 
-
+// //function base 
 // const App = () => {
 //   return (
 //     <div>
@@ -19,4 +10,29 @@ class App extends React.Component{
 //   );
 // };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const rootNode = document.getElementById('root');
+// const root = ReactDOM.createRoot(rootNode);
+// root.render(React.createElement(App));
+
+// class base component
+
+const React = require('react')
+const ReactDOM = require('react-dom/client');
+
+
+class App extends React.Component{
+    render() {
+      return <h1>Hello, World!</h1>;
+  }
+}
+
+const rootNode = document.getElementById('root');
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(App));
+
+/* 
+- ReactDOM.render is no longer supported in React 18.
+ 
+  ReactDOM.render(<App />, document.getElementById('root'));
+
+  */
