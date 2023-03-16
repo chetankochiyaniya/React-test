29c4d7cc877f5e29ffb8f953cc528054e033e187
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-16 10:23:46 +0530

    class base component in webpack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+const React = require('react')
+const ReactDOM = require('react-dom');
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-};
+class App extends React.Component{
+    render() {
+      return <h1>Hello, World!</h1>;
+  }
+}
+
+//function base 
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Hello, World!</h1>
+//     </div>
+//   );
+// };
 
 ReactDOM.render(<App />, document.getElementById('root'));
