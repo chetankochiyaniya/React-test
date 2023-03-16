5c8a9c402bd52d45bd8253f0a2bf04f01c35c1c2
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-16 11:09:51 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+  /* ===============================
+      Example 1
+      ================================= */
 
-// //function base 
-// const App = () => {
-//   return (
-//     <div>
-//       <h1>Hello, World!</h1>
-//     </div>
-//   );
-// };
 
-// const rootNode = document.getElementById('root');
-// const root = ReactDOM.createRoot(rootNode);
-// root.render(React.createElement(App));
+//function base component
+//=======================
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//function base 
+const App = ({name}) => {
+  return (
+    <div>
+      <h1>Hello, {name}</h1>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App name="chetan Kochiyaniya"/>);
+*/
+
 
 // class base component
+//=====================
 
+/*
 const React = require('react')
 const ReactDOM = require('react-dom/client');
 
 
 class App extends React.Component{
     render() {
-      return <h1>Hello, World!</h1>;
+      return <h1>Hello, {this.props.name}</h1>;
   }
-}
+} 
+ReactDOM.createRoot(document.getElementById('root')).render(<App name="chetan Kochiyaniya"/>);
+*/
 
-const rootNode = document.getElementById('root');
-const root = ReactDOM.createRoot(rootNode);
-root.render(React.createElement(App));
 
 /* 
 - ReactDOM.render is no longer supported in React 18.
@@ -36,3 +46,57 @@ root.render(React.createElement(App));
   ReactDOM.render(<App />, document.getElementById('root'));
 
   */
+
+
+
+
+  /* ===============================
+      Example 2
+      ================================= */
+
+
+
+//function base component
+//=======================
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = (props) => {
+  return (
+    <div>
+      <h1>Employees Details</h1>
+      <p>name: {props.edata.name}</p>
+      <p>age :  {props.edata.age}</p>
+      <p>no  : {props.edata.no}</p>
+    </div>
+  );
+};
+const Data = {name:"chetan kochiyaniya", age:"21", no: 6353074971 }
+ReactDOM.createRoot(document.getElementById('root')).render(<App edata={Data}/>);
+*/
+
+
+
+// class base component
+//=====================
+
+
+const React = require('react')
+const ReactDOM = require('react-dom/client');
+
+
+class App extends React.Component{
+    render() {
+      return <div>
+      <h1>Employees Details</h1>
+      <p>name: {this.props.edata.name}</p>
+      <p>age :  {this.props.edata.age}</p>
+      <p>no  : {this.props.edata.no}</p>
+    </div>;
+  }
+} 
+const Data = {name:"chetan kochiyaniya", age:"21", no: 6353074971 }
+ReactDOM.createRoot(document.getElementById('root')).render(<App edata={Data}/>);
+
