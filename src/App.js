d6d6b9c51cd8b788cc1aed289c5bf58ab7c4674a
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-17 09:51:10 +0530

    Handling Events and Props as function

--- src/App.js
+++ src/App.js
@@ -7,32 +7,38 @@
 //=====================
 
 
-// const React = require('react')
-// const ReactDOM = require('react-dom/client');
+const React = require('react')
+const ReactDOM = require('react-dom/client');
 
 
-// class App extends React.Component{
-//   state = {edata: ["chetan kochiyaniya",21,6353074971]}
+class App extends React.Component{
+  state = {edata: ["chetan kochiyaniya",21,6353074971]}
 
-//     render() {
-//       let list = this.state.edata
-//       list = list.map((item,index)=><List item={item} key={index}/>)
+    render() {
+      let list = this.state.edata
+      list = list.map((item,index)=><List item={item} key={index} handelOnDelete={this.onDelete}/>)
+    
+      return <div>
+      <h1>Employees Details</h1>
+      <ul>
+        {list}
+      </ul>
+    </div>;
+  }
+  onDelete =(item)=>{
+    const updatedlist = this.state.edata.filter((val)=>item!==val)
+    this.setState({edata:updatedlist})
+  }
+} 
 
-//       return <div>
-//       <h1>Employees Details</h1>
-//       <ul>
-//         {list}
-//       </ul>
-//     </div>;
-//   }
-// } 
-
-// class List extends React.Component{
-//   render(){
-//     return <li>{this.props.item}</li>
-//   }
-// }
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+class List extends React.Component{
+  
+  render(){
+    console.log(this.props)
+    return <><li>{this.props.item}</li><span onClick={()=>this.props.handelOnDelete(this.props.item)}>del</span></>
+  }
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 
 
@@ -45,23 +51,27 @@
 //=======================
 
 
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+// import React, { useState } from 'react';
+// import ReactDOM from 'react-dom/client';
 
-const App = () => {
-  const [data,setData] = useState({edata: ["chetan kochiyaniya",21,6353074971]})
-  const list =  data.edata.map((item,ind)=><List item={item} key={ind}/>)
+// const App = () => {
+//   const [data,setData] = useState({edata: ["chetan kochiyaniya",21,6353074971]})
+//   const onDelete  =(item)=>{
+//     const updatedList = data.edata.filter((val)=> item !== val)
+//     setData({edata:updatedList})
+//   }
 
-  return (
-    <div>
-      <h1>Employees Details</h1>
-      <ul>{list}</ul>
-    </div>
-  );
-};
+//   const list =  data.edata.map((item,ind)=><List item={item} key={ind} handelOnDelete={onDelete}/>)
 
+//   return (
+//     <div>
+//       <h1>Employees Details</h1>
+//       <ul>{list}</ul>
+//     </div>
+//   );
+// };
 
-const List =(props)=>{
-return <li>{props.item}</li>
-}
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// const List =(props)=>{
+// return <><li>{props.item}</li><span onClick={()=>props.handelOnDelete(props.item)}>del</span></>
+// }
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
