feb80fafbb4b83292e0beb44cc86665d8c2659fa
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-24 17:24:49 +0530

    Active NavLink Demo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ const NavaBar = () => {
     return <>
         <ul>
             {/**The NavLink is used when you want to highlight a link as active. 
-             * So, on every routing to a page, the link is highlighted according to the activeClassName . 
+             * So, on every routing to a page, the link is highlighted according to the activeclassname . 
              * Other Css style for NavLink 
              * Link is for links that need no highlighting. And a is for external links. */}
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/product/5">Product</NavLink></li>
+            <li><NavLink activeclassname="active" to="/">Home</NavLink></li>
+            <li><NavLink activeclassname="active" to="/about">About</NavLink></li>
+            <li><NavLink activeclassname="active" to="/product/5">Product</NavLink></li>
         </ul>
     </>
 }
