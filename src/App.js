af3f01c26aeab9298c24363dd57a19c239068553
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-27 14:58:44 +0530

    Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+
+import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import About from "./components/About";
 import Home from "./components/Home";
 import NavaBar from "./components/Navbar";
 import Product from "./components/Product";
 import User1 from "./components/User1";
 import User2 from "./components/User2";
 import Users from "./components/Users";
+// lazy load the About page
+const LazyAbout = React.lazy(()=> import("./components/About"))
 
 const App = () => {
   return (
@@ -13,7 +16,8 @@ const App = () => {
       <NavaBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
+        {/* lazy loading routes */}
+        <Route exact path="/about" element={<React.Suspense fallback="Loading ...."><LazyAbout/></React.Suspense>} />
         <Route exact path="/product/:id" element={<Product />} />
         <Route  path="/users" element={<Users />} >
           <Route path="user/1" element={<User1/>}/>
